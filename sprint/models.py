c94461d3298f38924262230c5689e366b0c15c50
themarcos98 <marcosdanielgg98@gmail.com>
2023-05-16 19:23:02 -0400

    Duración por Defecto del Sprint

--- sprint/models.py
+++ sprint/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from datetime import date
+from datetime import date, timedelta
 
 # Create your models here.
 
 class Sprints(models.Model):
     id_sprint = models.IntegerField(primary_key=True)
     nombre_sprint = models.CharField(max_length=10)
-    fecha_inicio = models.DateField(default = date.today)
-    fecha_fin_prev = models.DateField()
+    fecha_inicio = models.DateField(default = date.today().strftime("%Y-%m-%d"))
+    fecha_fin_prev = models.DateField(default = (date.today() + timedelta(days=14)).strftime("%Y-%m-%d"))
     fecha_fin = models.DateField(null=True)
 
     class Meta:
