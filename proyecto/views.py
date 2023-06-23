d40a61570a9a3ec9fb672dc96bb8f8ba3c58a1ac
themarcos98 <marcosdanielgg98@gmail.com>
2023-06-23 11:07:30 -0400

    Mostrar en pantalla el Backlog ID y mejoras en proyecto

--- proyecto/views.py
+++ proyecto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Proyectos
 from .forms import ProyectosForm
 
@@ -7,6 +7,7 @@ from .forms import ProyectosForm
 def lista_proyectos(request):
     proyectos = Proyectos.objects.all().order_by('id_proyecto')
     return render(request, 'lista_proyectos.html', {'proyectos': proyectos})
+
 
 # def usuarios_detail(request, pk):
 #     usuario = get_object_or_404(Usuarios, pk=pk)
@@ -19,8 +20,7 @@ def crear_proyecto(request):
         if form.is_valid():
             proyecto = form.save(commit=False)
             proyecto.save()
-            proyectos = Proyectos.objects.all().order_by('id_proyecto')
-            return render(request, 'lista_proyectos.html', {'proyectos': proyectos})
+            return redirect('proyecto:listaproyectos')
     else:
         form = ProyectosForm()
     return render(request, 'crear_proyecto.html', {'form': form})
@@ -33,8 +33,7 @@ def actualizar_proyecto(request, pk):
         if form.is_valid():
             proyecto = form.save(commit=False)
             proyecto.save()
-            proyectos = Proyectos.objects.all().order_by('id_proyecto')
-            return render(request, 'lista_proyectos.html', {'proyectos': proyectos})
+            return redirect('proyecto:listaproyectos')
     else:
         form = ProyectosForm(instance=proyecto)
     return render(request, 'actualizar_proyecto.html', {'form': form})
@@ -44,5 +43,4 @@ def eliminar_proyecto(request, pk):
     print(request, pk)
     proyecto = get_object_or_404(Proyectos, pk=pk)
     proyecto.delete()
-    proyectos = Proyectos.objects.all().order_by('id_proyecto')
-    return render(request, 'lista_proyectos.html', {'proyectos': proyectos})
+    return redirect('proyecto:listaproyectos')
